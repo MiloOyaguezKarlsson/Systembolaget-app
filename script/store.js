5175d886cd00b7e65294a39463677737ba267bda
MiloOyaguezKarlsson <mioy97001@utb.vaxjo.se>
2017-11-20 15:14:42 +0100

    hämtar och skriver ut addressen på store sidan

--- script/store.js
+++ script/store.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
     getStoreID();
+    loadStoreAddress();
 });
 //function för att hämta affärens id från url:en
 function getStoreID(){
-    var url = location.href;
-    var storeID = url.substring(url.indexOf("?id=") + 4);
+    var url = location.href; //hämta urlen
+    var storeID = url.substring(url.indexOf("?id=") + 4); //hämta ut delen efter "?id=" för att få fram id:et
 
-    document.getElementById("test").innerHTML += storeID;
     return storeID;
+}
+function loadStoreAddress(){
+    var url = "https://www.systembolaget.se/api/assortment/stores/xml";
+    var x;
+    $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/" + "https://www.systembolaget.se/api/assortment/stores/xml",
+        success: function(data) {
+            var id = getStoreID();
+            var stores = data.getElementsByTagName("ButikOmbud");
+            var max = stores.length;
+            console.log(max);
+            for (var i = 0; i < max; i++) {
+                var storeID = stores[i].childNodes[1].textContent;
+                if (storeID === id) {
+                    document.getElementById("address").innerHTML += stores[i].childNodes[3].textContent;
+
+                }
+            }
+        },
+        datatype: "xml",
+        error: function() {
+            alert("Error: Something went wrong with loading stores ");
+            console.log(status);
+        }
+    });
 }
